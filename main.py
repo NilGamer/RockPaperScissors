774e42e5ae06d914448f72ff2ff2e1dd67ea89a0
NilGamer <35883547+NilGamer@users.noreply.github.com>
2020-12-22 19:17:42 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+signs = [rock , paper , scissors]
+
+userInput = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+cmpGuess = random.randint(0,2)
+
+print(signs[userInput])
+print(f"Computer chose: \n {signs[cmpGuess]}")
+
+if(userInput == cmpGuess):
+  print("Draw")
+elif(userInput == 0 and cmpGuess == 1):
+  print("You Lose")
+elif(userInput == 1 and cmpGuess == 2):
+  print("You Lose")
+elif(userInput == 2 and cmpGuess == 0):
+  print("You Lose")
+else:
+  print("You Won")
